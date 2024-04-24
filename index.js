83489a2ac4c0dcd791f3316b41555d6e219b655b
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-04-25 02:55:51 +0600

    edit &update

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function run() {
 
     app.post('/coffee', async (req, res) => {
       const newCoffee = req.body;
-      console.log(newCoffee);
+      // console.log(newCoffee);
       //check data insert
       const result = await coffeeCollection.insertOne(newCoffee);
       res.send(result);
@@ -51,6 +51,37 @@ async function run() {
       const result = await coffeeCollection.deleteOne(query);
       res.send(result);
   })
+
+  app.get('/coffee/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await coffeeCollection.findOne(query);
+    res.send(result);
+})
+
+app.put('/coffee/:id', async(req, res) => {
+  const id = req.params.id;
+  const filter = {_id: new ObjectId(id)}
+  const options = { upsert: true };
+  const updatedCoffee = req.body;
+
+  const coffee = {
+      $set: {
+          name: updatedCoffee.name, 
+          quantity: updatedCoffee.quantity, 
+          supplier: updatedCoffee.supplier, 
+          taste: updatedCoffee.taste, 
+          category: updatedCoffee.category, 
+          details: updatedCoffee.details, 
+          photo: updatedCoffee.photo
+      }
+  }
+
+      const result = await coffeeCollection.updateOne(filter, coffee, options);
+      res.send(result);
+})
+
+  
 
 
 
